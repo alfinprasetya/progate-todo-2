5f3b46a909ceaa820da25d9b1c417f5ce2986869
Alfin Prasetya <alfin.prasetya24@gmail.com>
2024-06-10 18:08:38 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { useState } from 'react'
-import tasks from './data/initial-tasks'
-import Todos from './components/Todos'
-
+import { useState } from "react";
+import tasks from "./data/initial-tasks";
+import Todos from "./components/Todos";
 
 function App() {
-  const [todos, setTodos] = useState(tasks)
+  const [todos, setTodos] = useState(tasks);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
-  )
+  );
 }
 
 const styles = {
   container: {
-    textAlign: 'center',
-    padding: '12px',
+    textAlign: "center",
+    padding: "12px",
   },
   title: {
-    fontSize: '36px',
-    color: '#FF7F3E'
+    fontSize: "36px",
+    color: "#FF7F3E",
   },
-}
+};
 
-export default App
+export default App;
